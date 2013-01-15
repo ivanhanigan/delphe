36952c86086c92042170fbac46400c7158b1b38e
Ivan Hanigan <ivan.hanigan@gmail.com>
2013-01-15 15:38:21 +1100

    mod shp2pgis

--- R/shp2pgis.r
+++ R/shp2pgis.r
@@ -1,10 +1,20 @@
 # windows version.  creates a .bat file and prints a SQL command to run on the database once completed
-shp2pgis <- function(infile,d='postgis',u='postgres',host='localhost',srid=4283,schema='public',
+shp2pgis <- function(infile,d='postgis',u='postgres',host='localhost',srid=4283,schema='public',os='linux',
                      pgutils = 'C:\\pgutils\\'){
+  if(os == 'linux'){
+  cat('in a terminal run:\n\n')    
+  cat(paste("shp2pgsql -s ",srid," -D ",infile,".shp ",schema,".",infile," > ",infile,".sql",sep=""),"\n")
+  cat(paste("psql  -d ",d," -U ",u," -W -h ",host," -f ",infile,".sql",sep=""),"\n")
+
+  } else {
   cat(paste("\"",pgutils,"shp2pgsql\" -s ",srid," -D %1.shp ",schema,".%1 > %1.sql",sep=""),"\n")
   cat(paste("\"",pgutils,"psql\"  -d ",d," -U ",u," -W -h ",host," -f %1.sql",sep=""),"\n")
-  cat('make doshp.bat\n\n')
+  cat('copy these two lines to make your batch file with the name doshp.bat\n\n')
+  cat('now in the cmd line run:\n\n')
   cat(paste("doshp.bat ",infile,sep=""))
+  }
+  
+  cat('\n\nOnce uploaed to postgres you may want to run:\n\n')
   cat(paste("\n\nCREATE INDEX idx_",infile,"_the_geom ON ",schema,".",infile," USING gist(the_geom);\n",sep=""))
   cat(paste("VACUUM ANALYZE ",schema,".",infile,";\n",sep=""))
   
